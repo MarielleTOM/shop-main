c922513a0e1eb938c2f98955ffe034d40fde78b3
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2024-06-26 18:34:23 -0300

    26/06 - 2

--- src/app/Componentes/Main.js
+++ src/app/Componentes/Main.js
@@ -15,7 +15,7 @@ export default function Home() {
   useEffect(() => {
     const getProtucts = async () =>{
       try {
-        const response = await fetch("https://fakestoreapi.com/product")
+        const response = await fetch("https://fakestoreapi.com/products")
         const data = await response.json();
         setListaProdudt(data);
         setListaComplete(data);
--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,5 +1,7 @@
-export default function Product(){
+export default async function Product({params}){
+    const response= await fetch ("https://fakestoreapi.com/products" + params.id);
+    const data = await response.json();
     return(
-        <p>id: {params.id}</p>
+        <p>id: {data.title}</p>
     )
 }
